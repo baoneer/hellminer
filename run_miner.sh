62c3c24a8de53f6479830e5373e14f53bd5709b4
baoneer <baotranquoc0708@gmail.com>
2024-06-20 17:14:43 +0700

    run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -3,5 +3,4 @@
 
 # Example SSL URL
 # ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
-
 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RLLWFDdakPw9mcPQdfX6zHMvv53MLBsG5z.baonee -p x --cpu 1
